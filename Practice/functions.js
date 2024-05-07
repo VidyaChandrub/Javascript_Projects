6e112e14c879f0615173dbbed085d98b27aeafb8
Vidya <vidyac266@gmail.com>
2024-05-07 20:45:05 +0530

    function-as-exp

---
+++ Practice/functions.js
@@ -0,0 +1,14 @@
+// function greet(){
+//     console.log("Hello");
+// }
+
+// greet();
+
+//function as Expression also anonymus function
+
+var add = function(a,b){
+    return a+b;
+}
+
+var result = add(4,5);
+console.log(result);
